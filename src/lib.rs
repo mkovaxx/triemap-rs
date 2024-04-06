2f9f67d14c338bdf5e0e12b2e3224adad5c8f7e1
Mate Kovacs <mkovaxx@gmail.com>
2024-04-06 14:20:51 +0900

    generate wrapper type

--- src/lib.rs
+++ src/lib.rs
@@ -1,56 +1,37 @@
 use std::collections::BTreeMap;
 
-use proc_macro::TokenStream;
-use quote::quote;
+use quote::{format_ident, quote};
 use syn::{
     parse::{Parse, ParseStream},
-    parse_macro_input,
+    parse_macro_input, parse_quote,
     punctuated::Punctuated,
     DeriveInput, Ident, Result, Token,
 };
 
 #[proc_macro_attribute]
-pub fn trie_map(attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn trie_map(
+    attr: proc_macro::TokenStream,
+    item: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
     let args = parse_macro_input!(attr as Args);
-    let input = parse_macro_input!(item as DeriveInput);
+    let key = parse_macro_input!(item as DeriveInput);
 
-    let key_name = &input.ident;
-    let tm_name = args.type_map.get(key_name).expect(&format!(
+    let key_name = &key.ident;
+    let wrapper_name = args.type_map.get(key_name).expect(&format!(
         "the type mapping must contain an entry for {}",
         key_name,
     ));
+    let inner_name = format_ident!("Many_{}", wrapper_name);
+
+    let wrapper = generate_wrapper(wrapper_name, &inner_name, key_name);
+    let inner = generate_inner(&inner_name, wrapper_name);
 
     quote! {
-        #input
+        #key
 
-        enum #tm_name<V> {
-            Empty,
-            Single {
-                key: #key_name,
-                value: V,
-            },
-            Multi {
-                // TODO
-            },
-        }
+        #wrapper
 
-        impl<V> #tm_name<V> {
-            pub fn new() -> Self {
-                Self::Empty
-            }
-
-            pub fn single(key: #key_name, value: V) -> Self {
-                todo!()
-            }
-
-            pub fn get(&self, key: &#key_name) -> Option<&V> {
-                todo!()
-            }
-
-            pub fn insert(&mut self, key: #key_name, value: V) {
-                todo!()
-            }
-        }
+        #inner
     }
     .into()
 }
@@ -81,5 +62,128 @@ impl Parse for TypeMapEntry {
         let _ = input.parse::<Token![->]>()?;
         let value = input.parse::<Ident>()?;
         Ok(TypeMapEntry { key, value })
+    }
+}
+
+fn generate_wrapper(
+    wrapper_name: &Ident,
+    inner_name: &Ident,
+    key_name: &Ident,
+) -> proc_macro2::TokenStream {
+    parse_quote! {
+        pub enum #wrapper_name<V> {
+            Empty,
+            One(#key_name, V),
+            Many(Box<#inner_name>),
+        }
+
+        impl<V> #wrapper_name<V> {
+            pub fn new() -> Self {
+                Self::Empty
+            }
+
+            pub fn get(&self, key: &#key_name) -> Option<&V> {
+                match self {
+                    Self::Empty => None,
+                    Self::One(k, value) => {
+                        if k == key {
+                            Some(value)
+                        } else {
+                            None
+                        }
+                    }
+                    Self::Many(em) => em.get(key),
+                }
+            }
+
+            pub fn insert(&mut self, key: #key_name, value: V) {
+                // an offering to the Borrow Checker
+                let mut old_self = #wrapper_name::Empty;
+                std::mem::swap(self, &mut old_self);
+
+                match old_self {
+                    Self::Empty => {
+                        *self = Self::One(key, value);
+                    }
+                    Self::One(k, v) => {
+                        let mut em = Box::new(#inner_name::new());
+                        em.insert(k, v);
+                        em.insert(key, value);
+                        *self = Self::Many(em);
+                    }
+                    Self::Many(mut em) => {
+                        em.insert(key, value);
+                        *self = Self::Many(em);
+                    }
+                }
+            }
+
+            pub fn remove(&mut self, key: &#key_name) -> Option<V> {
+                // an offering to the Borrow Checker
+                let mut old_self = Self::Empty;
+                std::mem::swap(self, &mut old_self);
+
+                match old_self {
+                    Self::Empty => {
+                        *self = Self::Empty;
+                        None
+                    }
+                    Self::One(k, value) => {
+                        if k == *key {
+                            *self = Self::Empty;
+                            Some(value)
+                        } else {
+                            *self = Self::One(k, value);
+                            None
+                        }
+                    }
+                    Self::Many(mut em) => {
+                        let value = em.remove(key);
+                        // TODO: collapse into One when possible
+                        *self = Self::Many(em);
+                        value
+                    }
+                }
+            }
+
+            pub fn merge_with<F>(&mut self, that: Self, func: &mut F)
+            where
+                F: FnMut(&mut V, V),
+            {
+                // an offering to the Borrow Checker
+                let mut old_self = Self::Empty;
+                std::mem::swap(self, &mut old_self);
+
+                match old_self {
+                    Self::Empty => {
+                        *self = that;
+                    }
+                    Self::One(key, value) => {
+                        *self = that;
+                        self.insert(key, value);
+                    }
+                    Self::Many(mut em) => match that {
+                        Self::Empty => {}
+                        Self::One(key, value) => {
+                            em.insert(key, value);
+                            *self = Self::Many(em);
+                        }
+                        Self::Many(em_that) => {
+                            em.merge_with(*em_that, func);
+                            *self = Self::Many(em);
+                        }
+                    },
+                }
+            }
+        }
+    }
+}
+
+fn generate_inner(inner_name: &Ident, wrapper_name: &Ident) -> proc_macro2::TokenStream {
+    parse_quote! {
+        #[allow(non_camel_case_types)]
+        struct #inner_name<V> {
+            // TODO
+        }
     }
 }
