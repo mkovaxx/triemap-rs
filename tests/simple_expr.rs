af66740d76187c24dc3985cc0ffa03ff04a68e02
Mate Kovacs <mkovaxx@gmail.com>
2024-04-13 16:19:01 +0900

    add failing test

--- tests/simple_expr.rs
+++ tests/simple_expr.rs
@@ -28,3 +28,20 @@ fn test_insert_then_get() {
 
     assert_eq!(tm.get(&key), Some(&value));
 }
+
+#[test]
+fn test_insert_multi_then_get() {
+    let mut tm: ExprMap<char> = ExprMap::new();
+
+    let key = Expr::App(Expr::Var(0).into(), 42, Expr::Var(1).into());
+    let value = 'v';
+    tm.insert(key.clone(), value);
+
+    assert_eq!(tm.get(&key), Some(&value));
+
+    let key = Expr::Def(Expr::Var(0).into());
+    let value = 'a';
+    tm.insert(key.clone(), value);
+
+    assert_eq!(tm.get(&key), Some(&value));
+}
